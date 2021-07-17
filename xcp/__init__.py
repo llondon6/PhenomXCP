166353b9955cf1ca50a5cf5191bf37309f541bb5
Lionel London <lionel.london@ligo.org>
2021-07-16 21:02:33 -0400

    adding premilinary calibration data and package structure

---
+++ xcp/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+'''
+init for welsh puck aka pwca
+'''
+
+# Import usefuls
+from positive import *
+alert(yellow('Warm greetings from Welsh Puck (aka pwca)')+'.',fname='init')
+
+# Import local dependencies
+from . import ansatz
+from . import core
+from . import d
+from . import parameter_space_fits
+from . d import *
+from . ansatz import *
+from . core import *
+from . parameter_space_fits import *
+from . final_mass_polynomial_rho_function import *
+from . final_spin_polynomial_sigma_function import *
+from . final_spin_rational_sigma_function import *
