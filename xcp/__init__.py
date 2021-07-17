bf696467d74cd89d34be160045fdfb4edafc915b
Lionel London <lionel.london@ligo.org>
2021-07-16 21:05:02 -0400

    adding core and updating init

--- xcp/__init__.py
+++ xcp/__init__.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python
 '''
-init for welsh puck aka pwca
+init for python development of PhenomXCP
 '''
 
 # Import usefuls
 from positive import *
-alert(yellow('Warm greetings from Welsh Puck (aka pwca)')+'.',fname='init')
+alert(yellow('Warm greetings from XCP')+'.',fname='init')
 
 # Import local dependencies
-from . import ansatz
 from . import core
-from . import d
-from . import parameter_space_fits
-from . d import *
-from . ansatz import *
 from . core import *
-from . parameter_space_fits import *
-from . final_mass_polynomial_rho_function import *
-from . final_spin_polynomial_sigma_function import *
-from . final_spin_rational_sigma_function import *
