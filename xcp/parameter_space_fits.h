f9ece81c8d7fc96939da88d7d7771292c1454646
Lionel London <lionel.london@ligo.org>
2021-12-18 14:02:26 +0100

    adding automated python and C code for current model version

---
+++ xcp/parameter_space_fits.h
@@ -0,0 +1,20 @@
+
+
+/* Header file for IMRPhenomXCP's tuned parameters */
+
+// Header formatting for MU1
+static double IMRPhenomXCP_MU1( double theta, double eta, double a1 );
+// Header formatting for MU2
+static double IMRPhenomXCP_MU2( double theta, double eta, double a1 );
+// Header formatting for MU3
+static double IMRPhenomXCP_MU3( double theta, double eta, double a1 );
+// Header formatting for NU4
+static double IMRPhenomXCP_NU4( double theta, double eta, double a1 );
+// Header formatting for NU5
+static double IMRPhenomXCP_NU5( double theta, double eta, double a1 );
+// Header formatting for NU6
+static double IMRPhenomXCP_NU6( double theta, double eta, double a1 );
+// Header formatting for ZETA1
+static double IMRPhenomXCP_ZETA1( double theta, double eta, double a1 );
+// Header formatting for ZETA2
+static double IMRPhenomXCP_ZETA2( double theta, double eta, double a1 );
